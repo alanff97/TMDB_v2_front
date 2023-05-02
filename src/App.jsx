acf6257f54d9213ca5dbcb33ee049558517d31c2
alanff97 <alan.figueroa.cl@gmail.com>
2023-05-02 16:55:58 +0200

    config routes APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css';
-
+import Login from './components/Login';
 import { Movie } from './components/Movies';
 import { useSelector } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   const movies = useSelector((state) => state.movies);
   return (
     <div className="page">
       <main>
-        <Movie movies={movies} />
+        <Routes>
+          <Route path="/" element={<Movie movies={movies} />} />
+          <Route path="/search" element={<Movie movies={movies} />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </main>
     </div>
   );
