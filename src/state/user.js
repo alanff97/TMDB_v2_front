34e05438d16d220dee4204c02840cd435f7595d3
alanff97 <alan.figueroa.cl@gmail.com>
2023-05-05 16:51:51 +0200

    changes in state user and config

--- src/state/user.js
+++ src/state/user.js
@@ -1,13 +1,19 @@
 import { createAction, createReducer } from '@reduxjs/toolkit';
-
-const initialState = {};
+import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
 
 export const setUser = createAction('SET_USER');
+export const logOut = createAction('LOG_OUT');
+
+const token = Cookies.get('token');
+
+const user = token ? jwtDecode(token).user : {};
+
+const initialState = user;
 
 const userReducer = createReducer(initialState, {
-  [setUser]: (state, action) => {
-    return action.payload;
-  },
+  [setUser]: (state, action) => (state = action.payload),
+  [logOut]: (state, action) => (state = {}),
 });
 
 export default userReducer;
