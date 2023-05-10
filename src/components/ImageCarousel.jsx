1c37f4745821c9e56a3e7b2a7d5d9976e82b46e3
alanff97 <alan.figueroa.cl@gmail.com>
2023-05-10 22:45:03 +0200

    ImageCarousel component responsive

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,110 +1,104 @@
-import * as React from 'react';
 import PropTypes from 'prop-types';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
-function ImageCarousel({ movies }) {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = movies.length;
+import Carousel from 'react-material-ui-carousel';
+import {
+  Container,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+  Typography,
+} from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => (prevActiveStep + 1) % maxSteps);
-  };
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) =>
-      prevActiveStep === 0 ? maxSteps - 1 : prevActiveStep - 1
-    );
-  };
-
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
-
+function ImageCarousel({ content }) {
   return (
-    <Box sx={{ width: '100%', flexGrow: 1, mt: 2 }}>
-      <Paper
-        square
-        elevation={0}
+    <ThemeProvider theme={theme}>
+      <Container
+        maxWidth="xl"
         sx={{
           display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'background.default',
+          flexDirection: 'column',
+          mt: 2,
+          mb: 2,
+          p: 2,
         }}
       >
-        <Typography>{movies[activeStep].title}</Typography>
-      </Paper>
-      <div style={{ position: 'absolute', overflow: 'visible' }}>
-        {movies?.map((step, index) => (
-          <div
-            key={step.id}
-            style={{
-              position: 'absolute',
-              left: `${index * -100}%`,
-              width: '100%',
-              transition: 'all 0.3s ease-out',
-            }}
-          >
-            <Box
-              component="img"
+        <Carousel>
+          {content.map((content, i) => (
+            <Card
+              key={i}
               sx={{
-                display: 'block',
-                overflow: 'hidden',
+                display: 'flex',
+                flexDirection: 'row-reverse',
+                alignItems: 'flex-start',
+                backgroundColor: '#f8f8ff',
+                borderRadius: '10px',
+                [theme.breakpoints.down('md')]: {
+                  flexDirection: 'column',
+                },
               }}
-              src={step.image}
-              alt={step.title}
-            />
-          </div>
-        ))}
-      </div>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-    </Box>
+            >
+              <CardActionArea sx={{ flexGrow: 2 }}>
+                <CardMedia
+                  sx={{
+                    maxWidth: '100%',
+                    maxHeight: '100vh',
+                    objectFit: 'cover',
+                  }}
+                  component="img"
+                  image={content.backdrop_path}
+                  alt={content.title || content.name}
+                />
+              </CardActionArea>
+              <CardContent
+                sx={{
+                  flexGrow: 0,
+                  flexShrink: 0,
+                  maxWidth: '40%',
+                  m: '7vh auto',
+                  [theme.breakpoints.down('md')]: {
+                    m: 'auto',
+                    maxWidth: '100%',
+                  },
+                }}
+              >
+                <Typography gutterBottom variant="h5" component="div">
+                  {content.title || content.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {content.overview}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </Carousel>
+      </Container>
+    </ThemeProvider>
   );
 }
 ImageCarousel.propTypes = {
-  movies: PropTypes.arrayOf(
+  content: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
+      title: PropTypes.string,
+      name: PropTypes.string,
       release: PropTypes.string.isRequired,
       image: PropTypes.string.isRequired,
+      backdrop_path: PropTypes.string.isRequired,
+      overview: PropTypes.string.isRequired,
     })
   ).isRequired,
 };
